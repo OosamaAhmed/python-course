08f396f422df70e0c487dbf32ac786e168b5196f
Osama <osamaahmed01066000903@gmail.com>
2023-02-12 17:13:11 +0200

    day 1

---
+++ day1.py
@@ -0,0 +1,52 @@
+######################################################################################
+# Write a program that counts up the number of vowels [a, e, i, o, u]contained in the string.
+# s = input ("enter string here:- ")
+# count = 0
+# for i in s :
+#     # print(i)
+#     if i in 'aeiou':
+#         count+=1
+# print(count)
+# done---------------------------
+######################################################################################
+# Write a program that prints the number of times the string 'iti' occurs in anystring.
+# my_string = 'Hello iti ahmed alkatkoti iti osama iti '
+# print (my_string.count('iti')) # output = 3
+# done---------------------------
+######################################################################################
+# Write a program that remove all vowels from the input word and generate a brief version of it.
+# s = input("enter string here:- ")
+# v = 'aeiou'
+# result=''
+# for i in s :
+#     # print(i)
+#     if i not in v :
+#         result+=i
+# print(result)
+# done-----------------------
+# s = input("enter string here:- ")
+# v = 'aeiou'
+# for i in s :
+#     if i  in v :
+#         s=s.replace(i,"")
+# print(s)
+# done in second way ----------------
+#########################################################################
+# Writea program that prints the locations of "i" character  in any string you added.
+# s = input("enter string here:- ")
+# x=0
+# while x < len(s):
+#     if s[x] == 'i':
+#         print(x)
+#     x+=1
+# done---------------------------
+#########################################################################
+# Write a program that build a Mario pyramid like below:
+# rows = int(input("Enter the number : - "))
+# for i in range(1, rows+1):#range take integer value
+#     for space in range(1, (rows-i)+1):
+#         print(" ", end=' ')
+#     for symbol in range(1, i+1):
+#         print("*", end=' ')
+#     print("")
+# done-------------------------------------------
